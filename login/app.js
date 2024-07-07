652374e8f916846d6da505c47a5ba6e0955afa1a
심 <sacred9730@naver.com>
2024-07-07 14:47:54 +0900

    pc방

--- login/app.js
+++ login/app.js
@@ -1,7 +1,18 @@
+"use strict"
+
 const express = require("express");
 const app = express();
 
-app.get()
+
+app.get("/", (req, res) => {
+    res.send("루트화면입니다.");
+});
+
+app.get("/login", (req, res) => {
+    res.send("로그인화면");
+});
+
+
 
 app.listen(3000, () => {
     console.log("서버 가동")
