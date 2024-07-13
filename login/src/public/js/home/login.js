1d704dae7f5d1f86f4d469f505efba03022db66f
ITsks <sacred9730@naver.com>
2024-07-13 18:25:15 +0900

    공백수정

--- login/src/public/js/home/login.js
+++ login/src/public/js/home/login.js
@@ -13,7 +13,7 @@ function send_login(){
 fetch("/login",{
     method: "POST",
     headers: {
-        "Content-Type":"application/json",
+        "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
 });
