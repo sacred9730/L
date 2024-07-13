516e7f59a349198cf078ce27a7eaaa1e1e4f6328
ITsks <sacred9730@naver.com>
2024-07-13 18:24:29 +0900

    펫취 제이슨형태로 감쏴기

--- login/src/public/js/home/login.js
+++ login/src/public/js/home/login.js
@@ -1,10 +1,21 @@
+"use strict";
 
-document.addEventListener("DomContentLoaded", function(){
-    login();
-})
+let ejs_ID = document.querySelector("#input_ID");
+let ejs_PW = document.querySelector("#input_PW");
+let ejs_login = document.querySelector("#button_login");
+ejs_login.addEventListener("click", send_login);
+function send_login(){
+    let req = {
+        id: ejs_ID.value,
+        pw: ejs_PW.value,
+    };
 
-function login(){
-    document.querySelector("body > button").style.backgroundColor = "rgb(10,10,10)";
+fetch("/login",{
+    method: "POST",
+    headers: {
+        "Content-Type":"application/json",
+    },
+    body: JSON.stringify(req),
+});
+
 };
-console.log("하이");
-console.log("바이");
