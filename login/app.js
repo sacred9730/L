8617352e86f5477634b07f2c5f4a35b311ec2bc9
ITsks <sacred9730@naver.com>
2024-07-13 16:24:11 +0900

    ㅈ최적화

--- login/app.js
+++ login/app.js
@@ -5,12 +5,12 @@ const app = express();
 
 
 // 앱세팅 npm ejs 다운
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 
 
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home); // use ㅡ> 미들웨어를 등록해주는 메소드
 
 
