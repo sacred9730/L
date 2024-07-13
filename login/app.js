ff7a5c6f2fb364a5134cfe0ac4eab1e4efa23c3c
ITsks <sacred9730@naver.com>
2024-07-13 16:44:58 +0900

    프런트

--- login/app.js
+++ login/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-
+app.use(express.static(`${__dirname}/src/public`));
 
 const home = require("./src/routes/home");
 app.use("/", home); // use ㅡ> 미들웨어를 등록해주는 메소드
---
+++ login/src/public/js/home/login.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DomContentLoaded", function(){
+    login();
+})
+
+function login(){
+    document.querySelector("body > button").style.backgroundColor = "rgb(10,10,10)";
+};
+console.log("하이");
